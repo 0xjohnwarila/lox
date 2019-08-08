f49903bd91e8a3a60b0165102d21ef75ad82ed8a
JohnNWarila <johnwarila@gmail.com>
2019-08-07 18:44:15 -0700

    Added for loops, fixed scoping bug

--- v1/src/com/craftinginterpreters/lox/Environment.java
+++ v1/src/com/craftinginterpreters/lox/Environment.java
@@ -15,11 +15,11 @@ class Environment {
     this.enclosing = enclosing;
   }
 
-  public void define(String name, Object value) {
+  void define(String name, Object value) {
     values.put(name, value);
   }
 
-  public Object get(Token name) {
+  Object get(Token name) {
     if (values.containsKey(name.lexeme)) {
       return values.get(name.lexeme);
     }
@@ -37,6 +37,7 @@ class Environment {
 
     if (enclosing != null) {
       enclosing.assign(name, value);
+      return;
     }
 
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
--- v1/src/com/craftinginterpreters/lox/Interpreter.java
+++ v1/src/com/craftinginterpreters/lox/Interpreter.java
@@ -213,11 +213,10 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
-  public Void visitWhileStmt(Stmt.While stmt) { 
+  public Void visitWhileStmt(Stmt.While stmt) {
     while (isTruthy(evaluate(stmt.condition))) {
       execute(stmt.body);
     }
-    
     return null;
   }
 
