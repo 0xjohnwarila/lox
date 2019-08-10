fe64a23cfd6709419ea028d0f4cbea07b940f884
John Warila <johnwarila@gmail.com>
2019-08-09 17:20:09 -0700

    Added return values and put a bandaid on dynamic scoping/closures

--- v1/src/com/craftinginterpreters/lox/LoxFunction.java
+++ v1/src/com/craftinginterpreters/lox/LoxFunction.java
@@ -3,28 +3,37 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 class LoxFunction implements LoxCallable {
-    private final Stmt.Function declaration;
+  private final Stmt.Function declaration;
+  private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) { this.declaration = declaration; }
+  LoxFunction(Stmt.Function declaration, Environment closure) { 
+    this.declaration = declaration; 
+    this.closure = closure;
+  }
 
-    @Override
-    public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
-        for (int i = 0; i < declaration.params.size(); i++) {
-            environment.define(declaration.params.get(i).lexeme, arguments.get(i));
-        }
-
-        interpreter.executeBlock(declaration.body, environment);
-        return null;
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    Environment environment = new Environment(closure);
+    for (int i = 0; i < declaration.params.size(); i++) {
+      environment.define(declaration.params.get(i).lexeme, arguments.get(i));
     }
-
-    @Override
-    public int arity() {
-        return declaration.params.size();
+      
+    try {
+      interpreter.executeBlock(declaration.body, environment);
+    } catch (Return returnValue) {
+      return returnValue.value;
     }
+    
+    return null;
+  }
 
-    @Override
-    public String toString() {
-        return "<fn " + declaration.name.lexeme + ">";
-    }
+  @Override
+  public int arity() {
+    return declaration.params.size();
+  }
+
+  @Override
+  public String toString() {
+    return "<fn " + declaration.name.lexeme + ">";
+  }
 }
